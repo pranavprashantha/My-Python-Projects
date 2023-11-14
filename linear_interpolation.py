19089be5cbe83985a71d4e6396cbdbd44927d324
pranavprashantha <138083969+pranavprashantha@users.noreply.github.com>
2023-11-13 19:45:14 -0600

    Update linear_interpolation.py

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -5,18 +5,22 @@ Created on Tue Aug 29 15:05:01 2023
 @author: raopr
 """
 
-# By submitting this assignment, I agree to the following:
-# "Aggies do not lie, cheat, or steal, or tolerate those who do."
-# "I have not given or received any unauthorized aid on this assignment."
-#
-# Name: Pranav Rao
-# Section: 564
-# Assignment: 2.8 Lab
-# Date: 08/24/2023
-#
-#
-# YOUR CODE HERE
-#
+'''
+The first measurement was taken at time 𝑡 = 10 minutes, and the second was taken 45 minutes later. At the
+first measurement, the ISS was 2,027 kilometers past Houston, TX. At the second measurement, the ISS was
+23,027 kilometers past Houston.
+
+Write a program that determines, for any time between 10 and 55 minutes, where the ISS will be (in
+terms of kilometers past Houston). The time to evaluate at should be a variable in your program. The
+program should print both the time and the position at that time to the screen, with a line describing
+what is being output (see example output below). You should test your program at various times and
+make sure the results seem reasonable.
+
+For your final program that you submit, output the position at a time of 25 minutes. (Next week, we
+will see how you can read in numbers from a user, but for now, just assume it is a fixed number of
+minutes.)
+'''
+
 from math import *
 import numpy as np
 
