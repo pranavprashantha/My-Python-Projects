f5152684840eedb49a7c63087d18ac4ebb7fc9d3
pranavprashantha <138083969+pranavprashantha@users.noreply.github.com>
2023-11-13 19:52:58 -0600

    Update barcode_checker.py

--- barcode_checker.py
+++ barcode_checker.py
@@ -5,18 +5,29 @@ Created on Wed Nov  8 21:31:56 2023
 @author: raopr
 """
 
-# By submitting this assignment, I agree to the following:
-# "Aggies do not lie, cheat, or steal, or tolerate those who do."
-# "I have not given or received any unauthorized aid on this assignment."
-# Name: Pranav Rao
-# Section: 564
-# Assignment: 11.11 Lab
-# Date: 11/08/2023
-#
-#
-# YOUR CODE HERE
-#
+'''
+A barcode is valid if the digits satisfy a certain constraint. For example, take the 13-digit barcode
+1877455846014 and split the first 12 digits into two groups: (1,7,4,5,4,0) and (8,7,5,8,6,1).
+The first group contains every other digit starting with the first, and the second group contains every other
+digit starting with the second. Take the sum of the digits in the second group, and multiply it by 3. Add to that
+the sum of the digits in the first group. Subtract the last digit of that number from 10, and it should match the
+last digit of the barcode.
 
+Example math using barcode 1877455846014:
+Sum of first group = 1 + 7 + 4 + 5 + 4 + 0 = 21
+Sum of second group = 8 + 7 + 5 + 8 + 6 + 1 = 35
+Multiply second group by 3 = 35 x 3 = 105
+Add first group = 105 + 21 = 126
+Use the last digit and subtract from ten = 10 - 6 = 4
+4 is the last digit in the barcode, so it is valid
+
+Write a program named barcode_checker.py that takes as input a filename that contains many 13-digit
+barcodes. Have your program read the file, determine whether each barcode is valid, and write the valid
+barcodes to a new file named valid_barcodes.txt. Have your program output the total number of valid
+barcodes found using the example output below. You do not have to submit your valid_barcodes.txt file to
+zyBooks.
+
+'''
 #Takes in file input
 file_input = input("Enter the name of the file: ")
 file = open(file_input, "r")
