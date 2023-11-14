1d4fc2e5970c537af39aa1682352e4c369a89fdb
pranavprashantha <138083969+pranavprashantha@users.noreply.github.com>
2023-11-13 20:04:43 -0600

    Update word_puzzle.py

--- word_puzzle.py
+++ word_puzzle.py
@@ -1,12 +1,37 @@
-# By submitting this assignment, I agree to the following:
-#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
-#   "I have not given or received any unauthorized aid on this assignment."
-#
-# Name:         Pranav Rao
-# Section:      564
-# Assignment:   9.15
-# Date:         24 10 2023
+'''
+1. Create a function named get_valid_letters that takes in as an argument one string representing
+	a puzzle and returns one string of ten (10) unique letters found in the puzzle. For the example
+	puzzle above, your function should return the string RUEAMOSIKT
+2. Create a function named is_valid_guess that takes in as arguments two strings and returns a
+	Boolean value. The first string passed to the function is the string of valid letters from step 1; the
+	second string is a guess from the user. The function should return True only if the user’s guess
+	string contains exactly 10 unique letters from the valid letters string. This function does NOT
+	check if the user’s guess is the correct solution to the puzzle. For example, if the user’s guess is
+	TAKEOURSIM or IMAKETOURS, the function returns True; if the user’s guess is
+	IMAKEIMAKE, TAKEOUR, TAKEOURSIMM, or TAKEOURSBD, the function returns False
+3. Create a function named check_user_guess that takes in as arguments four integers
+	representing the dividend, quotient, divisor, and remainder (in that order) and returns a
+	Boolean value. The function should return True only if the following equation holds:
+	Dividend = Quotient * Divisor + Remainder. Otherwise, the function should return False
+4. Create a function named make_number that takes in as arguments two strings and returns one
+	integer. The first string passed to the function contains a word to be converted to an integer;
+	the second string is the user’s guess which should be used as a key to convert a word (the first
+	string) to its integer equivalent. For example, if the word is RUE and the user’s guess is
+	TAKEOURSIM, the integer equivalent of RUE is 653
+5. Create a function named make_numbers that takes in two arguments, both of which are strings,
+	and returns a tuple of four integers. The first string passed to the function is the puzzle string;
+	the second string is the user’s guess. Your make_numbers function should call your
+	make_number function four times to create the four integers, and then should return a tuple of
+	those four values that are the equivalent integers of the words representing the dividend,
+	quotient, divisor, and remainder (in that order). For the puzzle example shown above and the
+	user guess TAKEOURSIM, your function should return (659467, 653, 316, 571).
+6. Create a function named main that does not take in any arguments nor return any values. This
+	function should take as input from the user a puzzle string, print the puzzle, ask the user to
+	enter a guess, and output an appropriate message. Your main function should call your
+	get_valid_letters, is_valid_guess, make_numbers, and check_user_guess functions, as well as
+	the provided print_puzzle function. Format your output as shown below.
 
+'''
 def print_puzzle(puzzle):
     ''' Print puzzle as a long division problem. '''
     puzzle = puzzle.split(',')
